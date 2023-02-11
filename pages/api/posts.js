605e30eaf8e7ef2d8fbc7e800aec31cd0b65ad8a
alifaiizan <alifaizanahmad@gmail.com>
2023-02-11 11:03:48 -0800

    post api

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,61 +1,40 @@
-import formidable from 'formidable';
-import fs from 'fs';
-import path from 'path';
+// import formidable from 'formidable';
 import Config from '../../lib';
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
 
-const readResponse = (req) => new Promise((resolve, reject) => {
-  const form = new formidable.IncomingForm();
-  form.keepExtensions = true;
-  form.parse(req, (err, fields, files) => {
-    if (err) reject(err);
+// const readResponse = (req) => new Promise((resolve, reject) => {
+//   const form = new formidable.IncomingForm();
+//   form.keepExtensions = true;
+//   form.parse(req, (err, fields, files) => {
+//     if (err) reject(err);
 
-    // this handle is just returning singles file if multiple than handle multiple files
-    resolve({ fields, files });
-  });
-});
+//     // this handle is just returning singles file if multiple than handle multiple files
+//     resolve({ fields, files });
+//   });
+// });
 
-export default async function handler(req, next, res) {
+export default async function handler(req, res) {
   let myPost;
   let allPosts;
-  let result;
-  let fileLink;
-  let tfile;
   let data;
-  let buffer;
 
   const client = await Config.dbPromise;
-  const storage = await Config.storagePromise;
 
   const db = client.db('bijliwala');
 
   switch (req.method) {
     case 'POST':
-      data = await readResponse(req);
-      buffer = fs.createReadStream(path.join(process.cwd(), '/multi_page_pdf.pdf'));
-      console.log(buffer);
-      tfile = await storage.upload(
-        {
-          name: 'hello-worl',
-          size: 12,
-        },
-        buffer,
-      ).complete;
-      console.log('The file was uploaded!', tfile);
-      fileLink = await tfile.link();
-      data.fields.fileUrl = fileLink;
       myPost = await db.collection('posts').insertOne(data.fields);
-      result = res.json(myPost);
-      console.log(result);
+      res.json(myPost);
       break;
     case 'GET':
-      allPosts = await db.collection('posts').find({}).toArray();
-      res.json({ status: 200, data: allPosts });
+      allPosts = await db.collection('posts').find({}).limit(10).toArray();
+      res.json(allPosts);
       break;
     default:
   }
