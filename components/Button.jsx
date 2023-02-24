2dc8e1fa6578b4d42b76632f20cc8b75e336d47a
alifaiizan <alifaizanahmad@gmail.com>
2023-02-24 23:51:16 +0100

    button change

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,11 +1,11 @@
 
-const Button = ({ styles, btnName, handleClick }) => (
+const Button = ({ styles, btnName, handleClick, loading }) => (
   <button
     type="button"
     className={`gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${styles} `}
     onClick={handleClick}
   >
-    {btnName}
+    {loading ? 'loading' : btnName }
   </button>
 );
 
