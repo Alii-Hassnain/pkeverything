9d01c106d25ece99affce3ed5c7e17df2972f8de
alifaiizan <alifaizanahmad@gmail.com>
2023-02-26 20:09:50 +0100

    post by id

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,20 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
-const PostWithId = () => (
-  <div>PostWithId</div>
-);
+const PostWithId = () => {
+  const router = useRouter();
+  console.log(router.query);
+  const data = router.query;
+  return (
+    <div className="sm:px-4 p-12 py-5 w-full minmd:w-4/5 minmd:">
+      <p className="font-poppins text-color font-semibold text-xl">
+        {data.title}
+      </p>
+      <p className="font-poppins text-color minlg:text-xl text-sm">
+        {data.description}
+      </p>
+    </div>
+  );
+};
 
 export default PostWithId;
