4b4de4887825fb3ed6135f0ab77b0d35f792fd1d
alifaiizan <alifaizanahmad@gmail.com>
2023-02-26 20:25:44 +0100

    showing image

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,12 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
+
 import React from 'react';
 
 const PostWithId = () => {
   const router = useRouter();
-  console.log(router.query);
   const data = router.query;
+  console.log(router.query);
   return (
     <div className="sm:px-4 p-12 py-5 w-full minmd:w-4/5 minmd:">
       <p className="font-poppins text-color font-semibold text-xl">
@@ -13,6 +15,10 @@ const PostWithId = () => {
       <p className="font-poppins text-color minlg:text-xl text-sm">
         {data.description}
       </p>
+      <p className="font-poppins text-color minlg:text-xl text-sm">
+        {data.tags}
+      </p>
+      <img src={data.fileUrl} width={500} height={500} />
     </div>
   );
 };
