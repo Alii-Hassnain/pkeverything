e5262f2798d6a8ee34459d9613e344c31e882774
engrutman <engr.utman@gmail.com>
2023-03-21 01:49:39 +0500

    fixbug

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link'
 import { useTheme } from 'next-themes';
 import images from '../assets';
 import Button from './Button';
