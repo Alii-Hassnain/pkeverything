3083860a1afd6c0df93d48a9a7c131047021569d
alifaiizan <alifaizanahmad@gmail.com>
2023-04-07 23:07:39 +0200

    data actions

--- frontend/data/index.js
+++ frontend/data/index.js
@@ -1,8 +1,8 @@
 
 export const frequentActions = {
-  ElectricityBill: ['Electricity Bill', 'fas fa-newspaper fa-2xl', '/ConsumerCorner/OnlineBill'],
-  BillEstimator: ['Bill Estimator', 'fas fa-calculator fa-2xl', '/ConsumerCorner/BillEstimator'],
-  BillTarifs: ['Taxes & Tarifs', 'fas fa-file-invoice-dollar fa-2xl', '/ConsumerCorner/BillTarifs'],
+  ElectricityBill: ['Electricity Bill', 'fas fa-newspaper fa-2xl', '/OnlineBill'],
+  BillEstimator: ['Bill Estimator', 'fas fa-calculator fa-2xl', '/BillEstimator'],
+  BillTarifs: ['Taxes & Tarifs', 'fas fa-file-invoice-dollar fa-2xl', '/BillTarifs'],
 };
 
 //fas fa-user is a font awesome icon
