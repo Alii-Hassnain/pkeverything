73685cb57854ba6be877507a442068f473e375bd
alifaiizan <alifaizanahmad@gmail.com>
2023-07-05 11:22:45 +0200

    new con

--- frontend/pages/NewConnectionApplication.js
+++ frontend/pages/NewConnectionApplication.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const NewConnectionApplication = () => (
-  <div>NewConnectionApplication</div>
+  <div>
+    <Banner
+      name={router.pathname.split("/")[2]}
+      substyles="md:text-3xl sm:text-xl sx:text-xl text-left"
+      styles="justify-start mb-3 h-50 p-12 xs:p-4 md:h-40 rounded"
+    />
+    <p className="font-poppins text-color minlg:text-xl text-sm">
+      Here you can find Bills of all Electric companies of Pakistan
+    </p>
+  </div>
 );
 
 export default NewConnectionApplication;
